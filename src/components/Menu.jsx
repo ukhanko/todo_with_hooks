02ae803d875a0e4a8472500483bf7ae330c329da
aa.bobkov <aa.bobkov@jet.msk.su>
2019-01-08 19:48:50 +0300

    add react hooks in eslint

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Menu = () => (
-  <span>Hello</span>
+  <div>
+    <button
+      type="button"
+      className="btn btn-success"
+    >
+      {'Create'}
+    </button>
+    <button
+      type="button"
+      className="btn btn-danger"
+    >
+      {'Remove'}
+    </button>
+  </div>
 );
 
 export default Menu;
