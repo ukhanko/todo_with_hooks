27408b8bc7773eb1f0949b77ed4329dde119e120
aa.bobkov <aa.bobkov@jet.msk.su>
2019-01-13 16:14:04 +0300

    rewrite TaskAdd with hooks

--- src/components/TaskAdd.jsx
+++ src/components/TaskAdd.jsx
@@ -1,60 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class TaskAdd extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      content: '',
-    };
-  }
-
-  handleInputChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    const { onSubmit } = this.props;
-    event.preventDefault();
-    onSubmit(this.state);
-  }
-
-  render() {
-    const { title, content } = this.state;
-    return (
-      <form onSubmit={event => this.handleSubmit(event)}>
-        <div className="form-row m-2">
-          <input
-            value={title}
-            name="title"
-            className="form-control col-4 mr-1"
-            placeholder="Title"
-            type="text"
-            onChange={event => this.handleInputChange(event)}
-          />
-          <input
-            value={content}
-            name="content"
-            className="form-control col-4 mr-2"
-            placeholder="Content"
-            type="text"
-            onChange={event => this.handleInputChange(event)}
-          />
-          <button
-            type="submit"
-            className="btn btn-primary"
-            disabled={!title || !content}
-          >
-            {'Add'}
-          </button>
-        </div>
-      </form>
-    );
-  }
-}
+const TaskAdd = ({ onSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  return (
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      onSubmit({ title, content });
+    }}
+    >
+      <div className="form-row m-2">
+        <input
+          value={title}
+          name="title"
+          className="form-control col-4 mr-1"
+          placeholder="Title"
+          type="text"
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input
+          value={content}
+          name="content"
+          className="form-control col-4 mr-2"
+          placeholder="Content"
+          type="text"
+          onChange={event => setContent(event.target.value)}
+        />
+        <button
+          type="submit"
+          className="btn btn-primary"
+          disabled={!title || !content}
+        >
+          {'Add'}
+        </button>
+      </div>
+    </form>
+  );
+};
 
 TaskAdd.propTypes = {
   onSubmit: PropTypes.func.isRequired,
