35ebc322830b14227d5a1ba34b970e79ef67cef7
aa.bobkov <aa.bobkov@jet.msk.su>
2019-01-13 17:12:43 +0300

    rewrite app class with hooks

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,65 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid/v4';
 import Toolbar from './components/Toolbar';
 import Task from './components/Task';
 
 const LOCAL_STORE_NAME = 'todo-state';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [],
-    };
-  }
+const App = () => {
+  const [tasks, setTasks] = useState(localStorage[LOCAL_STORE_NAME]
+    ? JSON.parse(localStorage[LOCAL_STORE_NAME]).tasks
+    : []);
 
-  componentDidMount() {
-    const state = (localStorage[LOCAL_STORE_NAME])
-      ? JSON.parse(localStorage[LOCAL_STORE_NAME])
-      : {};
-    this.setState(state);
-  }
-
-  addHandler(values) {
-    const { tasks } = this.state;
+  const addHandler = (values) => {
     const id = uuid();
-    this.setState(
-      { tasks: [...tasks, { ...values, id }] },
-      () => {
-        localStorage[LOCAL_STORE_NAME] = JSON.stringify(this.state);
-      },
-    );
-  }
+    setTasks([...tasks, { ...values, id }]);
+  };
 
-  removeHandler(id) {
-    const { tasks } = this.state;
-    this.setState(
-      { tasks: tasks.filter(task => task.id !== id) },
-      () => {
-        localStorage[LOCAL_STORE_NAME] = JSON.stringify(this.state);
-      },
-    );
-  }
+  const removeHandler = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
 
-  render() {
-    const { tasks } = this.state;
-    return (
-      <div>
-        <Toolbar
-          addHandler={values => this.addHandler(values)}
-        />
-        <div className="d-flex flex-wrap align-items-start">
-          {tasks.map(el => (
-            <Task
-              key={el.id}
-              removeHandler={id => this.removeHandler(id)}
-              {...el}
-            />
-          ))}
-        </div>
+  useEffect(() => {
+    localStorage[LOCAL_STORE_NAME] = JSON.stringify({ tasks });
+  }, [tasks]);
+
+  return (
+    <div>
+      <Toolbar
+        addHandler={values => addHandler(values)}
+      />
+      <div className="d-flex flex-wrap align-items-start">
+        {tasks.map(el => (
+          <Task
+            key={el.id}
+            removeHandler={id => removeHandler(id)}
+            {...el}
+          />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
